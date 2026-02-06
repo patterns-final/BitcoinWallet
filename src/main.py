dc2d7fdd0f6b7756c5f4078f0e13e2a7d2271006
sanarskiluka <sanarskiluka2003@gmail.com>
2026-02-07 02:42:48 +0400

    feat: add entry point of app

--- src/main.py
+++ src/main.py
@@ -2,12 +2,19 @@ from __future__ import annotations
 
 from fastapi import FastAPI
 
+from src.api.routes import user_router
 from src.config import Settings
 from src.infra.database.migrate import run_migrations
 
 
 def create_app(settings: Settings) -> FastAPI:
     app = FastAPI(title="Bitcoin Wallet API")
+
+    app.include_router(user_router)
+
+    @app.get("/")
+    def root():
+        return {"message": "This is root, see /docs for swagger"}
 
     app.state.settings = settings
 
@@ -19,3 +26,14 @@ def create_app(settings: Settings) -> FastAPI:
 
 
 app = create_app(Settings())
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        app="src.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
