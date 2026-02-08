acdd5f4b110daee9743bad88cc6548ff0db39dbc
Nika Sardanadze <nsard22@freeuni.edu.ge>
2026-02-08 15:24:11 +0400

    add model for wallet

--- src/infra/models.py
+++ src/infra/models.py
@@ -12,3 +12,13 @@ class UserModel(Base):
     api_key = Column(String, unique=True, index=True, nullable=False)
 
     wallets = relationship("WalletModel", back_populates="user", cascade="all, delete-orphan")
+
+class WalletModel(Base):
+    __tablename__ = "wallets"
+
+    id = Column(String, primary_key=True, index=True)
+    address = Column(String, unique=True, index=True, nullable=False)
+    user_id = Column(String, ForeignKey("users.id"), nullable=False, index=True)
+    balance_satoshis = Column(Integer, nullable=False, default=0)
+
+    user = relationship("UserModel", back_populates="wallets")
